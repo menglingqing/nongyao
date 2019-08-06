e8c04006ee444c6bfc1dd4ede7bad33f3abfcc4b
孟令卿 <menglingqing@menglingqingdeMacBook-Pro.local>
2019-08-06 09:58:34 +0800

    use vue2-editor to article.body

--- admin/src/views/ArticleEdit.vue
+++ admin/src/views/ArticleEdit.vue
@@ -12,7 +12,7 @@
         <el-input v-model="model.title"></el-input>
       </el-form-item>
       <el-form-item label="详情">
-        <el-input v-model="model.body"></el-input>
+        <vue-editor v-model="model.body"></vue-editor>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
@@ -22,7 +22,12 @@
 </template>
 
 <script>
+import { VueEditor } from 'vue2-editor'
+
 export default {
+  components: {
+    VueEditor,
+  },
   data() {
     return {
       model: {},
