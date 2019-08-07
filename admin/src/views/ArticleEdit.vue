b83b4c06b4c97b60a2e2b8ff749d6d58f0704fff
menglingqing <menglingqing@github.com>
2019-08-07 09:12:30 +0800

    vue-editor add image & create ads page

--- admin/src/views/ArticleEdit.vue
+++ admin/src/views/ArticleEdit.vue
@@ -4,15 +4,19 @@
     <el-form label-width="120px" @submit.native.prevent="save">
       <el-form-item label="所属分类">
         <el-select v-model="model.categories" multiple>
-          <el-option v-for="item in categories" :key="item._id"
-            :label="item.name" :value="item._id"></el-option>
+          <el-option
+            v-for="item in categories"
+            :key="item._id"
+            :label="item.name"
+            :value="item._id"
+          ></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="标题">
         <el-input v-model="model.title"></el-input>
       </el-form-item>
       <el-form-item label="详情">
-        <vue-editor v-model="model.body"></vue-editor>
+        <vue-editor v-model="model.body" useCustomImageHandler @imageAdded="handleImageAdded"></vue-editor>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
@@ -22,16 +26,16 @@
 </template>
 
 <script>
-import { VueEditor } from 'vue2-editor'
+import { VueEditor } from "vue2-editor";
 
 export default {
   components: {
-    VueEditor,
+    VueEditor
   },
   data() {
     return {
       model: {},
-      categories: [],
+      categories: []
     };
   },
   props: {
@@ -58,6 +62,15 @@ export default {
     async fetchCategories() {
       const res = await this.$http.get(`rest/categories`);
       this.categories = res.data;
+    },
+
+    async handleImageAdded(file, Editor, cursorLocation, resetUploader) {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const res = await this.$http.post("upload", formData);
+      Editor.insertEmbed(cursorLocation, "image", res.data.url);
+      resetUploader();
     }
   },
   created() {
